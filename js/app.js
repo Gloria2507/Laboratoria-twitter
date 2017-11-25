d313233b3943983096abe371824253a87e853340
Gloria2507 <tuamiga_007@hotmail.com>
2017-11-25 18:18:39 -0500

    versión 2

--- js/app.js
+++ js/app.js
@@ -3,20 +3,33 @@ window.addEventListener('load', begin);
 var input = document.getElementById('click-id');
 var texarea = document.getElementById('twittext-id');
 var container = document.getElementById('container-id');
+var counte = document.getElementById('count');
+var maxText = 140;
 
 function begin() {
-  
+
   input.addEventListener('click', submit);
+  texarea.addEventListener('keyup', validar);
+}
+function validar(event) {
+  if (event.target.value) {
+    input.disabled = false;
+    counte.textContent = maxText - event.target.value.length;
+  } else {
+    input.disabled = true;
+    counte.textContent = maxText;
+  }
 }
 
-function submit(event){
+function submit(event) {
   event.preventDefault();
 
-  var text = texarea.value; 
-  
-  var div = document.createElement('div');
-  
-  div.textContent = text;
+  var text = texarea.value;
+  if (text) {
+    var div = document.createElement('div');
 
- container.appendChild(div); 
+    div.textContent = text;
+
+    container.appendChild(div);
+  }
 }
